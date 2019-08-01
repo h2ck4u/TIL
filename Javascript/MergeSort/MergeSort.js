d8f5a9d7eb5a25ad54da9b02ca55338d313bb18e
h2ck4u <h2ckforu@gmail.com>
2019-08-01 23:34:00 +0900

    :pencil: mergeSort!

---
+++ Javascript/MergeSort/MergeSort.js
@@ -0,0 +1,31 @@
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    let pivot = Math.floor(arr.length / 2);
+    let left = arr.slice(0, pivot);
+    let right = arr.slice(pivot, arr.length);
+    console.log(pivot, left, right);
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let result = [];
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+    while (left.length) {
+        result.push(left.shift());
+    }
+    while (right.length) {
+        result.push(right.shift());
+    }
+    return result;
+}
+
+console.log(mergeSort([5, 2, 4, 7, 6, 1, 3, 8])); // [1, 2, 3, 4, 5, 6, 7, 8]
--- Javascript/QuickSort/QuickSort.js
+++ Javascript/QuickSort/QuickSort.js
@@ -1,23 +1,43 @@
 function QuickSort(arr) {
 	if (arr.length === 0) {
 		return [];
-    }
+	}
 
 	const pivot = arr[0];
 	const len = arr.length;
 	const left = [];
-    const right = [];
-    
-    for(let i=1; i<len; i++) {
+	const right = [];
+
+	for (let i = 1; i < len; i++) {
 		if (arr[i] < pivot) {
 			left.push(arr[i]);
-        } else {
+		} else {
 			right.push(arr[i]);
-        }
-    }
+		}
+	}
 
 	return QuickSort(left).concat(pivot, QuickSort(right));
 }
 
+
+
+function qSort(arr) {
+	if (arr.length === 0) {
+		return [];
+	}
+	var left = [];
+	var right = [];
+	var pivot = arr[0];
+
+	for (var i = 1; i < arr.length; i++) {
+		if (arr[i] < pivot) {
+			left.push(arr[i]);
+		} else {
+			right.push(arr[i]);
+		}
+	}
+	return qSort(left).concat(pivot, qSort(right));
+}
+
 const assert = require('assert');
-assert.deepEqual(QuickSort([5, 3, 8, 4, 9, 1, 6, 2, 7]), [1,2,3,4,5,6,7,8,9]);
+assert.deepEqual(qSort([5, 3, 8, 4, 9, 1, 6, 2, 7]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
